2e3ef58636848e86959c03612bda3639a05fa12b
JRRL157 <jrrlino157@gmail.com>
2022-11-10 21:46:23 -0300

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!2");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso de Java");
+        curso.setDescricao("Aprendendo java!");
+        curso.setCargaHoraria(1);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Hoho");
+        mentoria.setDescricao("Hoho2");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso.toString());
+        System.out.println(mentoria.toString());
     }
 }
